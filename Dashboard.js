01948160004ab06374eaae8d4e8cd3fc4ee28951
GBUBEMI <105184327+Osimi-Gift@users.noreply.github.com>
2022-09-02 10:44:52 +0100

    Upgrading
    More features has been added, with Javascript validation, bootstrap features...

---
+++ Dashboard.js
@@ -0,0 +1,13 @@
+alert('connected');
+
+
+var input =document.getElementById('file').value;
+var container = document.getElementById('div');
+ function profile(){
+for (var i = 0; i < 10; i++) {
+  container.innerHTML= "i";
+}
+
+ }
+  
+
